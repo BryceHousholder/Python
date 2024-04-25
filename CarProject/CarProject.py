f293db17cb86a116ac400faac84307553fee1695
Bryce <brycehousholder@gmail.com>
2024-04-25 11:24:11 -0700

    Added CSV File

--- CarProject/CarProject.py
+++ CarProject/CarProject.py
@@ -1,1 +1,6 @@
-print('this is my car project file')
+# Bryce Housholder
+# Python Script which pulls data from the ODB-ii port on my 2017 Toyota Rav4
+import pandas as pd
+
+df = pd.read_csv('CarProject/ODBii CSV Test 1.csv')
+print(df.columns)
