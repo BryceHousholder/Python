c6a046742106af161dbae118352848ce70308ceb
Bryce <brycehousholder@gmail.com>
2024-04-25 15:54:02 -0700

    edit to read only columns 0-3
    -was reading NaN additional column

--- CarProject/CarProject.py
+++ CarProject/CarProject.py
@@ -2,6 +2,5 @@
 # Python Script which pulls data from the ODB-ii port on my 2017 Toyota Rav4
 import pandas as pd
 
-df = pd.read_csv('CarProject/ODBii CSV Test 1.csv',delimiter=';')
-
+df = pd.read_csv('CarProject/ODBii CSV Test 1.csv',delimiter=';',usecols=[0,1,2,3])
 print(df.head(5))
