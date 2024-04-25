9dc7c62ec4cbafbc87f39e164ad9404a13d242f2
Bryce <brycehousholder@gmail.com>
2024-04-25 12:03:40 -0700

    Added delimiter

--- CarProject/CarProject.py
+++ CarProject/CarProject.py
@@ -2,5 +2,6 @@
 # Python Script which pulls data from the ODB-ii port on my 2017 Toyota Rav4
 import pandas as pd
 
-df = pd.read_csv('CarProject/ODBii CSV Test 1.csv')
-print(df.columns)
+df = pd.read_csv('CarProject/ODBii CSV Test 1.csv',delimiter=';')
+
+print(df.head(5))
