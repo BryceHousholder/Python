52bfaeba239e2682f5543137a2a1e16096923ca2
Bryce <brycehousholder@gmail.com>
2024-04-25 19:33:15 -0700

    Calculated tIme stops for seconds and mins.

--- CarProject/CarProject.py
+++ CarProject/CarProject.py
@@ -3,4 +3,12 @@
 import pandas as pd
 
 df = pd.read_csv('CarProject/ODBii CSV Test 1.csv',delimiter=';',usecols=[0,1,2,3])
-print(df.head(5))
+#print(df.head(5))
+
+#Calculate Time Steps
+startSeconds = df.at[0,'SECONDS']
+endSeconds = df.at[df.index[-1],'SECONDS']
+totalSeconds = round(endSeconds - startSeconds,2)
+totalMins = round(totalSeconds/60,2)
+
+# Calculate Total Miles 
