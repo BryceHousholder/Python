bba8e9b7435b1523f50b55c6bdbc508ae2b385a4
Bryce <brycehousholder@gmail.com>
2024-04-25 22:23:22 -0700

    Tested total distance values - error in calculation

--- CarProject/CarProject.py
+++ CarProject/CarProject.py
@@ -1,6 +1,7 @@
 # Bryce Housholder
 # Python Script which pulls data from the ODB-ii port on my 2017 Toyota Rav4
 import pandas as pd
+import matplotlib as plt
 
 df = pd.read_csv('CarProject/ODBii CSV Test 1.csv',delimiter=';',usecols=[0,1,2,3])
 #print(df.head(5))
@@ -11,4 +12,14 @@ endSeconds = df.at[df.index[-1],'SECONDS']
 totalSeconds = round(endSeconds - startSeconds,2)
 totalMins = round(totalSeconds/60,2)
 
-# Calculate Total Miles 
+# Calculate total distance travelled
+distance = df['PID'] == 'Distance Travelled'
+for 
+if df['PID']=='Distance Travelled':
+
+
+#totalDistance = df[distance]['VALUES'].sum()
+#print(totalDistance)
+print(distance)
+
+#groupedDistance = df.groupby(['PID','Distance Travelled'])
