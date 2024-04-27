d75bc44ceff415d3db85388d3d96f04d98d0e8e7
Bryce <brycehousholder@gmail.com>
2024-04-26 20:57:16 -0700

    Uploaded Test 2 CSV, Fixed seconds values, calculated and plotted total trip distance, working on Instant MPH calulation.

--- CarProject/CarProject.py
+++ CarProject/CarProject.py
@@ -1,25 +1,45 @@
 # Bryce Housholder
 # Python Script which pulls data from the ODB-ii port on my 2017 Toyota Rav4
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('CarProject/ODBii CSV Test 1.csv',delimiter=';',usecols=[0,1,2,3])
-#print(df.head(5))
-
+df = pd.read_csv('CarProject/ODBii CSV Test 2.csv',delimiter=';',usecols=[0,1,2,3])
+# Fix seconds values
+df.eval("SECONDS = SECONDS - SECONDS[0]",inplace= True)
 #Calculate Time Steps
 startSeconds = df.at[0,'SECONDS']
 endSeconds = df.at[df.index[-1],'SECONDS']
 totalSeconds = round(endSeconds - startSeconds,2)
 totalMins = round(totalSeconds/60,2)
+# Sort and total trip distance in miles 
+sortedDistance = df[(df['PID'] == 'Distance travelled') & (df['VALUE'].notnull() )] 
+sortedDistanceValues = sortedDistance['VALUE'] # miles
+sortedDistanceMins = df.loc[(sortedDistance.index, 'SECONDS')] / 60 # mins
+totalDistance = round((sortedDistance.iloc[-1]['VALUE']),2) # miles
 
-# Calculate total distance travelled
-distance = df['PID'] == 'Distance Travelled'
-for 
-if df['PID']=='Distance Travelled':
+# plot Distance [Miles]/ Time [Min]
+plt.plot(sortedDistanceMins,sortedDistanceValues)
+plt.title('Distance Driven During Trip')
+plt.xlabel('Minutes')
+plt.ylabel('Miles')
+plt.show()
+
+# Instantaneous MPH calculation 
+#Hours = df['SECONDS'] / 3600
+#distanceDiff = sortedDistanceValues.diff()
+#distanceDiff = distanceDiff.fillna(0)
+#timeDiff = Hours.diff()
+#timeDiff = timeDiff.fillna(0)
+#InstantMPH = distanceDiff / timeDiff
+#InstantMPH.replace([float('inf'), float('-inf')], float('nan'), inplace=True)
+#print(MPH)
+#plt.plot(InstantMPH)
+#plt.show()
 
 
-#totalDistance = df[distance]['VALUES'].sum()
-#print(totalDistance)
-print(distance)
-#comment
-#groupedDistance = df.groupby(['PID','Distance Travelled'])
+
+
+    
+
+
+
